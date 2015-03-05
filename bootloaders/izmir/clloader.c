68a4bafa325bdc6aa959b322d319f5daa24bf12a
Krzysztof Sywula <Krzysztof.M.Sywula@intel.com>
2015-03-04 18:29:53 -0800

    sketch upload fix
    ported from internal repo (GAL-193)

--- bootloaders/izmir/clloader.c
+++ bootloaders/izmir/clloader.c
@@ -2721,7 +2721,13 @@ int clantonLoaderFSM(int argc, char * argv[])
 						ret = close(mystate.tty_from_host);
 						fprintf(stderr, "Bad connection to host  %d bytes available, ioctl return is %d\n",rd_bytes, ioctl_rc) ;
 						mystate.tty_from_host = -1 ;	
+						fprintf(stderr, "cable was probably disconnected\n");
+						//kill sketch then kill clloader
+						kill(mystate.slave_pid, SIGTERM);
+						exit(0);
 					}else{
+						fprintf(stderr, "host: %s\n", rbuf_from_host);
+						errors = 0;
 						/* TODO: bring out state change of BAUD/LINE and replace magic string */
 						if (strncmp(CL_LOADER_CMD_HOST_START_DOWNLOAD_CMD,
 							rbuf_from_host,
